f69cabede75289937113107c1caec771dbeb6c38
Prakash2216 <prakashsingh.iitr@gmail.com>
2017-05-25 23:19:15 +0530

    committing the change

--- src/com/ds/algo/graph/ShortestPathDijkstra.java
+++ src/com/ds/algo/graph/ShortestPathDijkstra.java
@@ -15,6 +15,40 @@ public class ShortestPathDijkstra {
 			sptSet[i] = false;
 		}
 		
+		dist[src]=0;
+		
+		for(int count=0; count<V-1; count++) {
+			//find the min dist vertex from the set of vertices not yet processed. 
+			int u = minDist(dist, sptSet);
+			sptSet[u]=true;
+			
+			//update the distance of adjacent vertex of u picked vertex.
+			for(int v=0; v<V; v++) {
+				//update dist[v] if v is not is sptSet. if there is an edge from u to v. the total weight of path from src to v
+				// through u is smaller than dist[v].
+				if(!sptSet[v] && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u]+graph[u][v] < dist[v])
+					dist[v] = dist[u]+graph[u][v];
+			}			
+		}
+		printDist(dist);
+	}
+	private void printDist(int[] dist) {
+		
+		System.out.println("Vertex Distance from source");
+		for(int v=0; v<V; v++) {
+			System.out.println(v+" "+dist[v]);
+		}
+	}
+	private int minDist(int[] dist, boolean[] sptSet) {
+		int min = Integer.MAX_VALUE;
+		int minIndex=-1;
+		for(int i=0; i<V; i++) {
+			if(!sptSet[i] && dist[i]<min) {
+				min=dist[i];
+				minIndex = i;
+			}				
+		}
+		return minIndex;
 	}
 	public static void main(String[] args) {
 		int [][]graph = {{0,10,20,0},
